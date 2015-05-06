cef84d42d17428d59a20e7ae00d2a3754e2ef317
Viacheslav Egorenkov <ve@poloniumarts.com>
2015-05-07 01:55:44 +0300

    [add]Prefs activity

--- flag/src/main/java/ru/egslava/flag/Prefs.java
+++ flag/src/main/java/ru/egslava/flag/Prefs.java
@@ -1,5 +1,6 @@
 package ru.egslava.flag;
 
+import org.androidannotations.annotations.sharedpreferences.DefaultInt;
 import org.androidannotations.annotations.sharedpreferences.SharedPref;
 
 /**
@@ -9,9 +10,19 @@ import org.androidannotations.annotations.sharedpreferences.SharedPref;
 @SharedPref(SharedPref.Scope.UNIQUE)
 public interface Prefs {
 
-    int t1n(); int t1m();
-    int t2n(); int t2m();
+    @DefaultInt(4) int t1n();
+    @DefaultInt(4) int t1m();
+    @DefaultInt(6) int t2n();
+    @DefaultInt(6) int t2m();
 
-    int time1(); int time2();
-    int list5Flags();
+    @DefaultInt(4) int e1n();
+    @DefaultInt(4) int e1m();
+    @DefaultInt(6) int e2n();
+    @DefaultInt(6) int e2m();
+
+    @DefaultInt(3) int secs1();     // time for first table
+    @DefaultInt(7) int secs2();     // time for second table
+
+    // how much flags from first table aren't shown in the second one (and will be added to five seria)
+    @DefaultInt(2) int list5Imgs();
 }
--- flag/src/main/java/ru/egslava/flag/PrefsActivity.java
+++ flag/src/main/java/ru/egslava/flag/PrefsActivity.java
@@ -1,8 +1,14 @@
 package ru.egslava.flag;
 
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.sharedpreferences.Pref;
+import org.androidannotations.api.sharedpreferences.IntPrefEditorField;
+import org.androidannotations.api.sharedpreferences.IntPrefField;
 
 /**
  * Created by egslava on 07/05/15.
@@ -10,4 +16,45 @@ import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_prefs)
 public class PrefsActivity extends Activity {
+
+    @Pref   Prefs_  prefs;
+
+    @ViewById   public EditText t1n, t1m, t2n, t2m, e1n, e1m, e2n, e2m, secs1, secs2, list5Imgs;
+
+    @Override   protected void onResume() {
+        super.onResume();
+        load(t1n, prefs.t1n());
+        load(t1m, prefs.t1m());
+        load(t2n, prefs.t2n());
+        load(t2m, prefs.t2m());
+        load(e1n, prefs.t1n());
+        load(e1m, prefs.t1m());
+        load(e2n, prefs.t2n());
+        load(e2m, prefs.t2m());
+        load(secs1, prefs.secs1());
+        load(secs2, prefs.secs2());
+        load(list5Imgs, prefs.list5Imgs());
+    }
+
+    @Override   protected void onPause() {
+        super.onPause();
+        final Prefs_.PrefsEditor_ e = prefs.edit();
+
+        save(t1n, e.t1n());
+        save(t1m, e.t1m());
+        save(t2n, e.t2n());
+        save(t2m, e.t2m());
+        save(e1n, e.t1n());
+        save(e1m, e.t1m());
+        save(e2n, e.t2n());
+        save(e2m, e.t2m());
+        save(secs1, e.secs1());
+        save(secs2, e.secs2());
+        save(list5Imgs, e.list5Imgs());
+        e.apply();
+    }
+
+    void save(TextView text, IntPrefEditorField i) { i.put(Integer.parseInt(text.getText().toString())); }
+    void load(TextView text, IntPrefField i) { text.setText( String.valueOf( i.get( ) ) ); }
+
 }
